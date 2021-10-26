b3f80f419693ae14b59407b2aa5da92e7ff3c96f
tesJS <horgetesf@gmail.com>
2021-10-26 18:12:39 +0300

    rearrange the App component

---
+++ src/AddPlayerSection.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './add-player-form.css';
+
+const AddPlayerSection = props => {
+  return (
+    
+      <div className='players-section'>
+            <span> Add Players</span>
+            
+          
+      <form onSubmit={props.submit}>
+        <label htmlFor='name'>Name*</label>
+        <br />
+        <input type='text' id='name' name='Name' placeholder='Your name...' />
+        <br />
+        <label>Elo-rating*</label>
+        <br />
+        <input
+          id='eloRating'
+          type='text'
+          name='EloRating'
+          placeholder='Your elo rating..'
+        />
+        <br />
+        <label>Club</label>
+        <br />
+        <input
+          id='club'
+          type='text'
+          name='Club'
+          placeholder='Your chess club..'
+        />
+        <br />
+
+        <input className='postButton' type='submit' name='Submit' />
+
+        <br />
+      </form>
+    </div>
+    
+  );
+};
+export default AddPlayerSection;
