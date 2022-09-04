de77fb83c9720dcc48842445e9fae440939ff6d1
Tesfaye Asfaw <horgetesf@gmail.com>
2022-09-04 18:09:17 +0300

    add login page and other edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,147 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
-import "./App.css";
+import React,{Fragment} from "react";
+
+import "./Main.css";
 import { useDispatch, useSelector } from "react-redux";
-import PlayerService from "./data/PlayerService";
-import Player from "./data/Player";
-import TourForm from "./components/TournamentForm";
-import AddPlayerSection from "./components/AddPlayerSection";
-import Home from "./components/Home";
-import Tournament from "./data/Tournament";
-import cloneDeep from "clone-deep";
-import ContactForm from "./components/ContactForm";
-import { httpActions } from "./store/httpReducer";
-import { loadPlayers, tournamentActions } from "./store/tournamentReducer";
+
+import Main from "./Main";
+
+import Login from "./components/Login";
+
+import { tournamentActions } from "./store/tournamentReducer";
 
 const App = () => {
-  const state = useSelector((state) => state.tournament);
+  const localState = useSelector((state) => state.tournament);
   const dispatch = useDispatch();
-
-  const DOMstrings = {
-    inputName: "#name",
-    inputElo: "#eloRating",
-    inputClub: "#club",
-  };
-  console.log("====================================");
-  console.log("Tournament State");
-  console.log(state);
-  console.log("====================================");
-  // run once all components are loaded to displa players list from the database or show errors if not succeeded
-
-  //ajax request is sent by PlayersService to Postgres database to fetch players data
-  // Here I use Spring boot for backend control program so Backend programs must run first to fetch data
-  // Otherwise Network error is thrown
-  useEffect(() => {
-    if (state.players.length < 2) dispatch(loadPlayers());
-  }, []);
-
-  const submitPlayerHandler = (event) => {
-    event.preventDefault();
-
-    let inputName = document.querySelector(DOMstrings.inputName);
-    let inputElo = document.querySelector(DOMstrings.inputElo);
-    let inputClub = document.querySelector(DOMstrings.inputClub);
-
-    let player = new Player(inputName.value, inputElo.value, inputClub.value);
-
-    PlayerService.postPlayer(player).catch((error) => {
-      dispatch(httpActions.displayError(error.message));
-    }); // save it to database
-
-    if (inputName.value !== "" && inputElo.value !== "") {
-      dispatch(tournamentActions.addPlayer(player));
-      inputName.value = "";
-      inputElo.value = "";
-      inputClub.value = "";
-    } else if (inputName.value === "" && inputElo.value === "") {
-      window.alert("Player's name and elo-rating are required!");
-    }
-  };
-
-  //Save tournament form data to the Redux Store but not yet to Tournament table in chesstourDB
-  // which will be done at the end of the tournament by saveButtonHandler in LowerSection Button
-
-  const submitTourHandler = (event) => {
-    event.preventDefault();
-    let tourForm = {};
-    let tourdetailsField = document.querySelector("#tournamentDesc");
-    let noplayersField = document.querySelector("#noplayers");
-    let noroundsField = document.querySelector("#rounds");
-    let touridField = document.querySelector("#tourid");
-
-    let noplayers = cloneDeep(parseInt(noplayersField.value));
-    let tourdetails = cloneDeep(tourdetailsField.value);
-    let norounds = cloneDeep(noroundsField.value);
-    let tourid = cloneDeep(touridField.value);
-
-    tourForm = new Tournament(noplayers, tourdetails, norounds, tourid);
-    tourdetailsField.value = "";
-    noplayersField.value = "";
-    noroundsField.value = "";
-    touridField.value = "";
-
-    dispatch(tournamentActions.addTournamentForm(tourForm));
-  };
+  dispatch(tournamentActions.addTournamentForm());
 
   return (
-    <div className="App">
-      <Router>
-        <div className="header items">
-          <h2> Chess Tournament Software </h2>
-          <ul className="App-header">
-            <li>
-              <Link style={{ color: "blue", textDecoration: "none" }} to="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/tour"
-                style={{ color: "blue", textDecoration: "none" }}
-              >
-                Tournament Form
-              </Link>
-            </li>
-            <li>
-              <Link to="/add" style={{ color: "blue", textDecoration: "none" }}>
-                Add
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/contact"
-                style={{ color: "blue", textDecoration: "none" }}
-              >
-                Contact Us
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="body items">
-          <Switch>
-            <Route exact path="/" render={(props) => <Home {...props} />} />
-            <Route
-              exact
-              path="/add"
-              render={(props) => (
-                <AddPlayerSection {...props} submit={submitPlayerHandler} />
-              )}
-            />
-
-            <Route
-              exact
-              path="/tour"
-              render={(props) => (
-                <TourForm {...props} submitTourForm={submitTourHandler} />
-              )}
-            />
-
-            <Route exact path="/contact" component={ContactForm} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <Fragment>
+      {localState.login && <Main />}
+      {!localState.login && <Login />}
+    </Fragment>
   );
 };
 
