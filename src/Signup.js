124fc874ed3d9051fe22d3316b4c71fd9d1b0fa8
tesJS <horgetesf@gmail.com>
2022-09-19 07:42:08 +0300

    edited  signup

--- src/Signup.js
+++ src/Signup.js
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { tournamentActions, checkSignupUser } from "./store/tournamentReducer";
 import User from "./data/User";
+import "./Main.css";
 
 function Signup(props) {
   const dispatch = useDispatch();
+  let errorLabel = useRef();
+  const [error, setError] = useState("");
   let passwordsMatch = false;
   const localState = useSelector((state) => state.tournament);
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
+
+  useEffect(() => {
+    console.log("Signup useEffect");
+    console.log(localState.httpErrorMessage);
+    setError(localState.httpErrorMessage);
+  }, [dispatch, localState.httpError]);
 
   function usernameHandler(event) {
     setUserName(event.target.value);
@@ -26,9 +35,11 @@ function Signup(props) {
     event.preventDefault();
     let user = new User(username, password, "GUEST");
 
-    if (password.length > 3 && password.value === confirmPassword.value) {
-      dispatch(checkSignupUser(user));
-    }
+    if (password !== confirmPassword) {
+      setError("Passwords do not match!");
+    } else if (password.length < 3) {
+      setError("Please enter loner password!");
+    } else dispatch(checkSignupUser(user));
   };
   const backbuttonHandler = () => {
     dispatch(tournamentActions.signup(false));
@@ -67,6 +78,9 @@ function Signup(props) {
           type="submit"
           value={"Signup"}
         ></input>
+        <label ref={errorLabel} id="signup-errorLabel">
+          {error}
+        </label>
       </form>
       <input
         onClick={backbuttonHandler}
