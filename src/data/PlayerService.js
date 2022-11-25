9bcd3a14d12a098f6829b6e0085d1f3dfe5e9211
Tesfaye Asfaw <horgetesf@gmail.com>
2022-11-25 14:22:50 +0200

    endpoints edited

--- src/data/PlayerService.js
+++ src/data/PlayerService.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 
 
-//const TOURNAMENT_REST_API_URL = "http://16.171.44.60:8080/tour";
-//const PLAYERS_REST_API_URL = "http://16.171.44.60:8080/player";
+const TOURNAMENT_REST_API_URL = "http://16.171.44.60:8080/tour";
+const PLAYERS_REST_API_URL = "http://16.171.44.60:8080/player";
 
-const PLAYERS_REST_API_URL = 'http://localhost:8080/player'; 
+/* const PLAYERS_REST_API_URL = 'http://localhost:8080/player'; 
 const TOURNAMENT_REST_API_URL = 'http://localhost:8080/tour';
-
+ */
 class PlayerService {
 
     
