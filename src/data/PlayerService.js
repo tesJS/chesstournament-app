f147460d7279e251bd3ea947836b49d6af8a2dcc
Tesfaye Asfaw <horgetesf@gmail.com>
2022-11-28 16:50:37 +0200

    url edits

--- src/data/PlayerService.js
+++ src/data/PlayerService.js
@@ -1,6 +1,4 @@
-import axios from 'axios'
-
-
+import axios from "axios";
 
 const TOURNAMENT_REST_API_URL = "http://16.171.44.60:8080/tour";
 const PLAYERS_REST_API_URL = "http://16.171.44.60:8080/player";
@@ -9,45 +7,26 @@ const PLAYERS_REST_API_URL = "http://16.171.44.60:8080/player";
 const TOURNAMENT_REST_API_URL = 'http://localhost:8080/tour';
  */
 class PlayerService {
+  async postPlayer(player) {
+    const data = await axios.post(PLAYERS_REST_API_URL + "/api/add", player);
+    return data;
+  }
 
-    
+  async getPlayers(username) {
+    const response = await axios.get(
+      PLAYERS_REST_API_URL + "/api/list/" + username
+    );
+    const data = await response.data;
+    return data;
+  }
 
-     async postPlayer(player){
-        
-       const data= await axios.post(PLAYERS_REST_API_URL+'/api/add', player);
-       return data;
-          
-    }
+  async postTournament(tour) {
+    axios.post(TOURNAMENT_REST_API_URL + "/api/save", tour);
+  }
 
-    
-    
-    async  getPlayers(username) {
-            const response=  await axios.get(PLAYERS_REST_API_URL+"/api/list/"+username);
-            const data= await response.data;            
-            return data;
-        } 
-
-
-
-
-
-
-    async postTournament(tour){
-    
-       axios.post(TOURNAMENT_REST_API_URL+"/api/save", tour);
-     
-          
-    }
-
-    async postTournamentResult(tourResult){
-    
-     axios.post(TOURNAMENT_REST_API_URL+"api/result/save", tourResult);
-     
-          
-    }
-    
-
-    
+  async postTournamentResult(tourResult) {
+    axios.post(TOURNAMENT_REST_API_URL + "/api/result/save", tourResult);
+  }
 }
 
 export default new PlayerService();
--- src/data/TournamentService.js
+++ src/data/TournamentService.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const TOURNAMENT_REST_API_URL = "http://16.171.44.60:8080/tour/";
+const TOURNAMENT_REST_API_URL = "http://16.171.44.60:8080/tour";
 //const TOURNAMENT_REST_API_URL = "http://localhost:8080/tour/";
 
 class TournamentService {
   async checkTourId(tourid) {
     const response = await axios.get(
-      TOURNAMENT_REST_API_URL + "checkTourid/" + tourid
+      TOURNAMENT_REST_API_URL + "/checkTourid/" + tourid
     );
     const data = await response.data;
 
